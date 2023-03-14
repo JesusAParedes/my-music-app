f6ecfef73ddbb12e5ab9dfa54feba99328fc9b1d
JesusAParedes <paredesjesus93@gmail.com>
2023-03-13 22:06:04 -0500

    start on project by adding nav bar and two inputs plus button to page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
 
-function App() {
+export default function ButtonAppBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My Music App
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+      <form>
+        <TextField id="standard-basic" label="Username*" variant="standard"  />
+        <TextField id="standard-basic" label="Password*" variant="standard"  />
+        <Button color="inherit">Login</Button>
+      </form>
+      
+    </Box>
+    
   );
 }
 
-export default App;
