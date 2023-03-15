69b31d8eef6eb6e506fb218822af230ed71a9a1f
JesusAParedes <paredesjesus93@gmail.com>
2023-03-14 21:16:24 -0500

    add css to move inputs & button to middle of page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,20 +6,24 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './App.css';
+
+const theme = createTheme({
+      primary: {
+
+      },
+      secondary: {
+        main: '#1976d2',
+      },
+})
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            
+          <IconButton>
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             My Music App
@@ -27,12 +31,14 @@ export default function ButtonAppBar() {
           
         </Toolbar>
       </AppBar>
-      <form>
-        <TextField id="standard-basic" label="Username*" variant="standard"  />
-        <TextField id="standard-basic" label="Password*" variant="standard"  />
-        <Button color="inherit">Login</Button>
+      <form className='form' style={{textAlign: 'center', display: 'flex'}}>
+        <TextField id="standard-basic" label="Username*" variant="standard" className='TextField' />
+        
+        <TextField id="standard-basic" label="Password*" variant="standard" className='TextField' />
+        <ThemeProvider theme={theme}>
+        <Button style={{backgroundColor: '#1976d2', padding: '0px 75px'}} variant='contained' className='Login' >Login</Button>
+        </ThemeProvider>
       </form>
-      
     </Box>
     
   );
