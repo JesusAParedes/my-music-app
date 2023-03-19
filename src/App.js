add067e621bbec8bf55ce86691a35f7406421d67
JesusAParedes <paredesjesus93@gmail.com>
2023-03-19 16:46:26 -0500

    create component folder with navbar and dashboard files. move navbar to component and start on dashboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
 import TextField from '@mui/material/TextField';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import NavBar from './components/NavBar';
+
 
 const theme = createTheme({
       primary: {
-
       },
       secondary: {
         main: '#1976d2',
@@ -19,27 +16,33 @@ const theme = createTheme({
 })
 
 export default function ButtonAppBar() {
+  const[loggedIn, setLoggedIn] = useState(false);
+
+  const handleClick = () => {
+    let status = loggedIn === false ? true : false;
+    setLoggedIn(status)
+  }
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            My Music App
-          </Typography>
-          
-        </Toolbar>
-      </AppBar>
+    <div>
+      {<NavBar />}
+      {loggedIn ? <Dashboard /> :
       <form className='form' style={{textAlign: 'center', display: 'flex'}}>
-        <TextField id="standard-basic" label="Username*" variant="standard" className='TextField' />
-        
-        <TextField id="standard-basic" label="Password*" variant="standard" className='TextField' />
-        <ThemeProvider theme={theme}>
-        <Button style={{backgroundColor: '#1976d2', padding: '0px 75px'}} variant='contained' className='Login' >Login</Button>
-        </ThemeProvider>
-      </form>
-    </Box>
+      <TextField id="standard-basic" label="Username*" variant="standard" className='TextField' />
+      
+      <TextField id="standard-basic" label="Password*" variant="standard" className='TextField' />
+      <ThemeProvider theme={theme}>
+      
+      <Button 
+      style={{backgroundColor: '#1976d2', padding: '0px 75px'}} 
+      variant='contained' 
+      className='Login'
+      onClick={handleClick}
+      >Login</Button>
+      </ThemeProvider>
+    </form> 
+    }
+    </div>
     
   );
 }
